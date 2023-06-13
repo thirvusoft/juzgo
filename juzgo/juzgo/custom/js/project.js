d23dcd211408ea5c0f7c59ed4a7e500a37c37dbc
sibikumarkuppusamy <sibi.kumar@thirvusoft.in>
2023-06-13 15:41:21 +0530

    feat:check list

--- juzgo/juzgo/custom/js/project.js
+++ juzgo/juzgo/custom/js/project.js
@@ -2,7 +2,16 @@ frappe.ui.form.on('Project', {
     onload:function (frm) {
         setTimeout(() => {
             $("[data-doctype='Expense Claim']").hide();
-            // $("[class='col-md-4']").hide();
         }, 10);
+    }
+})
+frappe.ui.form.on('Quotation Copy', {
+    quotation_copy_items_add:function (frm,cdt,cdn) {
+        frappe.model.set_value(cdt,cdn,"attached_by",frappe.session.user)
+    }
+})
+frappe.ui.form.on('Supplier Final Copies', {
+    supplier_final_copies_add:function (frm,cdt,cdn) {
+        frappe.model.set_value(cdt,cdn,"attached_by",frappe.session.user)
     }
 })
