d886cf03c570db602b9a4f9608afc56e745da530
Ponnusamy <95607086+Ponnusamy1-V@users.noreply.github.com>
2023-07-03 16:20:16 +0530

    Update attach_formatter.js

--- juzgo/public/attach_formatter.js
+++ juzgo/public/attach_formatter.js
@@ -1,7 +1,17 @@
-frappe.form.formatters['Attach'] = function(value, df) {
-    if (!value)
-        return ''
-let filename = value.substring(value.lastIndexOf("/") + 1);
-
-return filename || value
+$(document).ready(function () {
+    frappe.form.formatters['Attach'] = function(value, df) {
+        if (!value)
+            return ''
+    let filename = value.substring(value.lastIndexOf("/") + 1);
+    
+    return filename || value
+    }
+    
+    frappe.form.formatters['Attach Image'] = function(value, df) {
+        if (!value)
+            return ''
+    let filename = value.substring(value.lastIndexOf("/") + 1);
+    
+    return filename || value
+    }
 }
