8684e21a172b90585c453ac6bedf7f6737b0a29e
Ponnusamy <95607086+Ponnusamy1-V@users.noreply.github.com>
2023-07-03 16:21:22 +0530

    Update juzgo.bundle.js

--- juzgo/public/juzgo.bundle.js
+++ juzgo/public/juzgo.bundle.js
@@ -1,3 +1,5 @@
+import ".js/attach_formatter";
 import "../templates/web_form/interview/error.html";
 import "../templates/web_form/interview/already_responded.html";
+import "../templates/web_form/interview/not_opened_or_closed.html";
 import "../templates/web_form/interview/not_opened_or_closed.html";
