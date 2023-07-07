dc897acd77ce17593092986261b5fab1cfadb25e
sibikumarkuppusamy <sibi.kumar@thirvusoft.in>
2023-07-07 10:50:37 +0530

    ref table

--- juzgo/juzgo/doctype/hotel_details/hotel_details.js
+++ juzgo/juzgo/doctype/hotel_details/hotel_details.js
@@ -67,3 +67,15 @@ frappe.ui.form.on('Hotel Details', {
         }
     },
 });
+
+frappe.ui.form.on('Reference Table', {
+	open_url: function(frm,cdt,cdn){
+        let row = locals[cdt][cdn]
+        if(!row.url){
+            frappe.throw('Enter URL in Row '+row.idx+' .')
+        }
+        else{
+        window.open(row.url, '_blank')
+        }
+    },
+})
