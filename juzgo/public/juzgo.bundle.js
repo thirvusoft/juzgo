888558fe75a4b947c189fb5109b80264ad2b510c
root <root@ip-172-26-3-86.ap-south-1.compute.internal>
2023-07-14 07:46:33 +0000

    live changes

--- juzgo/public/juzgo.bundle.js
+++ juzgo/public/juzgo.bundle.js
@@ -7,9 +7,7 @@ import "../templates/web_form/interview/not_opened_or_closed.html";
 $(document).on('app_ready', function () {
     frappe.realtime.off("show_notification")
     frappe.realtime.on("show_notification", (args)=>{
-        var log = frappe.model.get_new_doc('Error Log')
         function show_notifications(notif){
-            frappe.call({method:"frappe.client.save", args:{doc:log}})
             var clicks = new Notification(strip_html(notif.subject), { body: strip_html(notif.email_content), icon: "https://app.juzgoholidays.com/files/juzgo%20length%20logo.jpg"});
             clicks.addEventListener('click', function(){
                 frappe.set_route("Form", notif.document_type, notif.document_name);
