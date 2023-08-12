b774b7cf186dc09fb115ce4103a194778e3aeca4
root <root@ip-172-26-3-86.ap-south-1.compute.internal>
2023-08-12 12:03:14 +0530

    live changes

--- juzgo/juzgo/custom/js/project.js
+++ juzgo/juzgo/custom/js/project.js
@@ -824,3 +824,15 @@ function remove_add_fc(frm,cdt,cdn){
         
         frm.save()
 }
+
+frappe.ui.form.on('Reference Table', {
+	open_url: function(frm,cdt,cdn){
+        let row = locals[cdt][cdn]
+        if(!row.url){
+            frappe.throw('Enter URL in Row '+row.idx+' .')
+        }
+        else{
+        window.open(row.url, '_blank')
+        }
+    },
+})
