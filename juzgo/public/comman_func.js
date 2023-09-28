7e4ba7526a0699c3a8582eb22c34c16d2574f145
root <root@ip-172-26-3-86.ap-south-1.compute.internal>
2023-09-28 19:02:28 +0530

    live changes

--- juzgo/public/comman_func.js
+++ juzgo/public/comman_func.js
@@ -9,3 +9,14 @@ frappe.ui.form.on('Reference Table', {
         }
     },
 })
+frappe.ui.form.on('R and D Attachment', {
+	open_url: function(frm,cdt,cdn){
+        let row = locals[cdt][cdn]
+        if(!row.url){
+            frappe.throw('Enter URL in Row '+row.idx+' .')
+        }
+        else{
+        window.open(row.url, '_blank')
+        }
+    },
+})
