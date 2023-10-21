6aba698c5781520665877640e448aa135faeec80
root <root@ip-172-26-3-86.ap-south-1.compute.internal>
2023-10-21 13:01:24 +0530

    project

--- juzgo/juzgo/custom/py/branch.py
+++ juzgo/juzgo/custom/py/branch.py
@@ -12,12 +12,13 @@ def validate(self,action):
                     frappe.throw(f"#Row:{i.idx} is for Sales Invoice Naming Series. It's character count must be less then 14 then only GST Portal allow.")
         else:
             frappe.throw(f"#Row:{i.idx} is Duplicate.Remove this row.")
-    for i in company_wise:
-        set1 = set(["Sales Invoice","Purchase Invoice","Journal Entry","Payment Entry"])
-        set2 = set(company_wise[i])
-        missing_values = set1 - set2
-        if missing_values:
-            throw_msg = throw_msg + f"-> {i} company's Missing Naming Series:{str(missing_values)}<br>"
-    
-    if throw_msg != '':
-        frappe.throw(throw_msg)
+    if self.set_default_naming_series == 0:
+        for i in company_wise:
+            set1 = set(["Sales Invoice","Purchase Invoice","Journal Entry","Payment Entry"])
+            set2 = set(company_wise[i])
+            missing_values = set1 - set2
+            if missing_values:
+                throw_msg = throw_msg + f"-> {i} company's Missing Naming Series:{str(missing_values)}<br>"
+        
+        if throw_msg != '':
+            frappe.throw(throw_msg)
