e66fa9f25140b934004cba4bf344206a1507b740
creative-paramu <pparameshwari@thirvusoft.in>
2024-02-16 18:33:59 +0530

    detailing_page_customize

--- juzgo/juzgo/doctype/detailing_page/detailing_page.js
+++ juzgo/juzgo/doctype/detailing_page/detailing_page.js
@@ -11,3 +11,42 @@ frappe.ui.form.on('Detailing Page', {
         // $(wrapper).append("<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' />");
     }
 });
+
+frappe.ui.form.on('Visa Supplier Table', {
+    destination: function(frm, cdt, cdn){
+        if(frm.doc.ca_form){
+            frappe.call({
+                method: "juzgo.juzgo.doctype.detailing_page.detailing_page.ca_form_details",
+                args: {ca_form:frm.doc.ca_form},
+                callback: function(r){
+                    let child_count = r.message[0].no_of_childrens +r.message[0].child_without_bed
+                    setTimeout(() => {
+                        frappe.model.set_value(cdt,cdn, "travel_from_dates" , r.message[0].travel_start_date );
+                        frappe.model.set_value(cdt,cdn, "travel_to_dates" , r.message[0].travel_end_date );
+                        frappe.model.set_value(cdt,cdn, "no_of_adults" , r.message[0].no_of_adult );
+                        frappe.model.set_value(cdt,cdn, "no_of_child" , child_count );
+                    }, 300)
+                    
+                }
+            })
+        }
+    },
+    supplier: function(frm, cdt, cdn){
+        if(frm.doc.ca_form){
+            frappe.call({
+                method: "juzgo.juzgo.doctype.detailing_page.detailing_page.ca_form_details",
+                args: {ca_form:frm.doc.ca_form},
+                callback: function(r){
+                    let child_count = r.message[0].no_of_childrens +r.message[0].child_without_bed
+                    setTimeout(() => {
+                        frappe.model.set_value(cdt,cdn, "travel_from_dates" , r.message[0].travel_start_date );
+                        frappe.model.set_value(cdt,cdn, "travel_to_dates" , r.message[0].travel_end_date );
+                        frappe.model.set_value(cdt,cdn, "no_of_adults" , r.message[0].no_of_adult );
+                        frappe.model.set_value(cdt,cdn, "no_of_child" , child_count );
+                    }, 300)
+                    
+                }
+            })
+        }
+    }
+})
