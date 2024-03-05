78b0b5390a767651c29c1ee645c8d218ae7b93c9
creative-paramu <pparameshwari@thirvusoft.in>
2024-03-05 15:44:47 +0530

    options adding in html table

--- juzgo/juzgo/doctype/worksheet/worksheet.js
+++ juzgo/juzgo/doctype/worksheet/worksheet.js
@@ -2,6 +2,520 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Worksheet', {
+	onload: async function(frm){
+		let data=`<table style="font-size:14px; border:1px solid black;width:100%">
+
+			<tr style="font-weight:bold; border:1px solid black; padding:5px;">
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+					Desc
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+					Option 1
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+					Option 2
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+					Option 3
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+					Option 4
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+					Option 5
+				</center>
+				</td>
+			</tr>
+			<tr style="font-weight:bold; border:1px solid black; padding:5px;">
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+					Inclusions
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+				</center>
+				</td>
+				<td style="border:1px solid black; padding:5px;">
+				<center>
+				</center>
+			</td>
+			</tr>
+			`
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.accomadation",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Accommodation Details
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.sighting_details",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Sightseeing Details
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.vehicle_details",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Vehicle Details
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.optional_tours",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Optional tours
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.complementory",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Complimentaries
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.miscellenous_details",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Miscellaneous
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			data +=
+			`<tr style="font-weight:bold; border:1px solid black; padding:5px;">
+				<td style="border:1px solid black; padding:5px;" colspan="6">
+					<center>
+						Net Selling Prices
+					</center>
+				</td>
+			</tr>`
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.adult_double",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Adult Double
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.adult_single",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Adult Single
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.adult_triple",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							Adult Triple
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.cnb",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							CNB
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+			await frappe.call({
+				method:"juzgo.juzgo.doctype.worksheet.worksheet.cwb",
+				args:{doc:frm.doc},
+				callback: function(r){
+					data += 			`
+					<tr style="border:1px solid black; padding:5px;">
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+							CWB
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[0]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[1]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[2]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[3]+`
+						</center>
+						</td>
+						<td style="border:1px solid black; padding:5px;">
+						<center>
+						`+ r.message[4]+`
+						</center>
+						</td>
+
+					`
+				}
+			})
+		data += `</table>`
+		frm.get_field("html").$wrapper.html( data);
+	}
 	// refresh: function(frm) {
 
 	// }
