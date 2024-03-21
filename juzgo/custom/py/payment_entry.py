c0323ea34e304535df3b54207f5de143cc106efd
Vengadesh-TS <venkadeshpandian@thirvusoft.in>
2024-03-21 19:29:47 +0530

    bug fix

--- juzgo/custom/py/payment_entry.py
+++ juzgo/custom/py/payment_entry.py
@@ -4,7 +4,12 @@ import frappe
 # Define function to append Payment Receive record to Project
 def make_entry(doc, method):
     if doc.doctype == "Payment Entry" and doc.payment_type == "Receive":
-        project = frappe.get_doc("Project", doc.references[0].project)  # Assuming doc.project contains the Project ID
+        project_name = ""
+        if doc.references and doc.references[0].project:
+            project_name = doc.references[0].project
+        elif doc.project:
+            project_name = doc.project
+        project = frappe.get_doc("Project", project_name)  # Assuming doc.project contains the Project ID
         project.append("custom_payment_receive_table", {
             'payment_entry': doc.name,
             'grand_total': doc.total_allocated_amount,
@@ -18,7 +23,12 @@ def make_entry(doc, method):
 
 
     if doc.doctype == "Payment Entry" and doc.payment_type == "Pay":
-        project = frappe.get_doc("Project", doc.references[0].project)  # Assuming doc.project contains the Project ID
+        project_name = ""
+        if doc.references and doc.references[0].project:
+            project_name = doc.references[0].project
+        elif doc.project:
+            project_name = doc.project
+        project = frappe.get_doc("Project", project_name)  # Assuming doc.project contains the Project ID
         project.append("custom_payment_pay_table", {
             'payment_entry': doc.name,
             'grand_total': doc.total_allocated_amount,
