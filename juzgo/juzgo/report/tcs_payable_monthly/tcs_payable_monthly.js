be68f15e6157137f7636431db0c578dddbbe9ff6
rooban <rooban24samuel@gmail.com>
2024-03-27 19:06:30 +0530

    tcs payable monthly report

--- juzgo/juzgo/report/tcs_payable_monthly/tcs_payable_monthly.js
+++ juzgo/juzgo/report/tcs_payable_monthly/tcs_payable_monthly.js
@@ -3,35 +3,36 @@
 /* eslint-disable */
 
 frappe.query_reports["TCS Payable Monthly"] = {
-	"filters": [
-		{
-			"fieldname":"company",
-			"label": __("Company"),
-			"fieldtype": "Link",
-			"options": "Company",
-			"default": frappe.defaults.get_default('company')
-		},
-		{
-			"fieldname":"supplier",
-			"label": __("Supplier"),
-			"fieldtype": "Link",
-			"options": "Supplier",
-		},
-		{
-			"fieldname":"from_date",
-			"label": __("From Date"),
-			"fieldtype": "Date",
-			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
-			"reqd": 1,
-			"width": "60px"
-		},
-		{
-			"fieldname":"to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"default": frappe.datetime.get_today(),
-			"reqd": 1,
-			"width": "60px"
-		}
-	]
+    "filters": [
+        {
+            "fieldname":"company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_default('company')
+        },
+        {
+            "fieldname":"customer",
+            "label": __("Customer"),
+            "fieldtype": "Link",
+            "options": "Customer",
+          
+        },
+        {
+            "fieldname":"from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+            "reqd": 1,
+            "width": "60px"
+        },
+        {
+            "fieldname":"to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "reqd": 1,
+            "width": "60px"
+        }
+    ]
 }
